16693d19782243c3313fe90660f28f0226d058a9
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-02 20:29:14 +1100

    1.[ADD]:add real time MVP Matrix calculations 2.[ADD]:add degree control by using keyboad A/a for counterclockwise D/d clockwise

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,75 @@
 #include <Eigen/Eigen>
 #include <Render.hpp>
 #include <Triangle.hpp>
+#include <Tools.hpp>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
 int main() {
-  SoftRasterizer::RenderingPipeline render(800, 600);
+          int key = 0;
+          int frame_count = 0;
+          float degree = 0.0f;
+
+  SoftRasterizer::RenderingPipeline render(1000, 1000);
 
   /*set up all vertices*/
   std::vector<Eigen::Vector3f> pos{
       {0.f, 0.5f, 0.f}, {-0.5f, -0.5f, 0.f}, {0.5f, -0.5f, 0.f}};
-  render.loadVertices(pos);
 
   /*set up all shading param(colours)*/
   std::vector<Eigen::Vector3f> color{
       {1.0f, 0.f, 0.f}, {0.f, 1.0f, 0.f}, {0.f, 0.f, 1.0f}};
-  render.loadColours(color);
 
   /*set up all indices(faces)*/
   std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+  render.loadVertices(pos);
+  render.loadColours(color);
   render.loadIndices(ind);
 
-  int key = 0;
-  int frame_count = 0;
   while (key != 27) {
     /*clear both shading and depth!*/
     render.clear(SoftRasterizer::Buffers::Color |
                  SoftRasterizer::Buffers::Depth);
 
+    /*Rotations*/
+    auto rotations = SoftRasterizer::Tools::calculateRotationMatrix(
+              /*axis=*/Eigen::Vector3f(0, 0, 1),
+              /*degree=+ for Counterclockwise;- for Clockwise*/degree
+    );
+
+    /*Model Matrix*/
+    auto model = SoftRasterizer::Tools::calculateModelMatrix(
+              /*transform=*/Eigen::Vector3f(0.0f, 0.0f, 0.f),
+              /*rotations=*/rotations,
+              /*scale=*/Eigen::Vector3f(1.f, 1.f, 1.f)
+    );
+
+    /*View Matrix*/
+    auto view = SoftRasterizer::Tools::calculateViewMatrix(
+              /*eye=*/Eigen::Vector3f(0.0f, 0.0f, 0.9f),
+              /*center=*/Eigen::Vector3f(0.0f, 0.0f, 0.0f),
+              /*up=*/Eigen::Vector3f(0.0f, 1.0f, 0.0f)
+    );
+
     /*draw lines*/
+    render.setModelMatrix(model);
+    render.setViewMatrix(view);
+    //render.setProjectionMatrix();
     render.display(SoftRasterizer::Primitive::LINES);
 
-    key = cv::waitKey(10);
+    key = cv::waitKey(1);
+    if (key == 'a' || key =='A') {
+              degree -= 10.0f;
+    }
+    else if (key == 'd' || key == 'D') {
+              degree += 10.0f;
+    }
+
+    /*reset the degree*/
+    auto delta = degree - 360.f;
+    if (delta >= -0.00000001f && delta <= 0.00000001f) {
+              degree = 0.0f;
+    }
   }
   return 0;
 }
