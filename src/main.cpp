cf361d51f69a93f5350c2071a1b56cce46805fd8
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-16 20:10:11 +1100

    1.[ADD]:add scene elements and light elements 2.[ADD]:add scene to render class and activiate rendering process

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,52 @@ int main() {
   int key = 0;
   float degree = 0.0f;
 
-  SoftRasterizer::RenderingPipeline render(1024, 1024);
+  auto render = std::make_shared< SoftRasterizer::RenderingPipeline>(1024, 1024); //Create Render Main Class
+  auto scene = std::make_shared< SoftRasterizer::Scene>("TestScene");                     //Create A Scene
+  scene->addGraphicObj(
+            CONFIG_HOME "examples/models/spot/spot_triangulated_good.obj", "spot",
+            Eigen::Vector3f(0.f, 1.f, 0.f), degree, Eigen::Vector3f(-0.35f, 0.0f, 0.0f),
+            Eigen::Vector3f(0.3f, 0.3f, 0.3f));
 
-  render.addShader("shader",
+  scene->addGraphicObj(
+            CONFIG_HOME "examples/models/Crate/Crate1.obj", "Crate",
+            Eigen::Vector3f(0.f, 1.f, 0.f), degree, Eigen::Vector3f(0.3f, 0.0f, 0.0f),
+            Eigen::Vector3f(0.2f, 0.2f, 0.2f));
+
+  scene->addShader("spot_shader",
                    CONFIG_HOME "examples/models/spot/spot_texture.png",
                    SoftRasterizer::SHADERS_TYPE::TEXTURE);
 
-  render.addGraphicObj(
-      CONFIG_HOME "examples/models/spot/spot_triangulated_good.obj", "spot",
-      Eigen::Vector3f(0.f, 1.f, 0.f), degree, Eigen::Vector3f(0.f, 0.0f, 0.0f),
-      Eigen::Vector3f(0.3f, 0.3f, 0.3f));
+  scene->addShader("crate_shader",
+            CONFIG_HOME "examples/models/Crate/crate1.png",
+            SoftRasterizer::SHADERS_TYPE::TEXTURE);
 
-  render.startLoadingMesh("spot");
-  render.bindShader2Mesh("spot", "shader");
+  scene->startLoadingMesh("spot");
+  scene->startLoadingMesh("Crate");
+  scene->bindShader2Mesh("spot", "spot_shader");
+  scene->bindShader2Mesh("Crate", "crate_shader");
+
+  /*Add Light To Scene*/
+  auto light1 = std::make_shared<SoftRasterizer::light_struct>();
+  light1->position = Eigen::Vector3f{ 0.9, 0.9, -0.9f };
+  light1->intensity = Eigen::Vector3f{ 100, 100, 100 };
+
+  scene->addLight("Light1", light1);
+
+  /*Register Scene To Render Main Frame*/
+  render->addScene(scene);
 
   while (key != 27) {
 
     /*clear both shading and depth!*/
-    render.clear(SoftRasterizer::Buffers::Color |
+    render->clear(SoftRasterizer::Buffers::Color |
                  SoftRasterizer::Buffers::Depth);
 
     /*Model Matrix*/
-    render.setModelMatrix(
+    scene->setModelMatrix(
         "spot",
         SoftRasterizer::Tools::calculateModelMatrix(
-            /*transform=*/Eigen::Vector3f(0.f, 0.0f, 0.0f),
+            /*transform=*/Eigen::Vector3f(0.f, 0.f, 0.f),
             /*rotations=*/
             SoftRasterizer::Tools::calculateRotationMatrix(
                 /*axis=*/Eigen::Vector3f(0.f, 1.f, 0.f),
@@ -43,18 +64,18 @@ int main() {
             /*scale=*/Eigen::Vector3f(0.3f, 0.3f, 0.3f)));
 
     /*View Matrix*/
-    render.setViewMatrix(
+    scene->setViewMatrix(
         /*eye=*/Eigen::Vector3f(0.0f, 0.0f, 0.9f),
         /*center=*/Eigen::Vector3f(0.0f, 0.0f, 0.0f),
         /*up=*/Eigen::Vector3f(0.0f, 1.0f, 0.0f));
 
     /*Projection Matrix*/
-    render.setProjectionMatrix(
+    scene->setProjectionMatrix(
         /*fov=*/45.0f,
         /*near=*/0.1f,
         /*far=*/100.0f);
 
-    render.display(SoftRasterizer::Primitive::TRIANGLES);
+    render->display(SoftRasterizer::Primitive::TRIANGLES);
 
     key = cv::waitKey(0);
     if (key == 'a' || key == 'A') {
