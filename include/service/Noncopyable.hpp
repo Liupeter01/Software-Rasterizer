1182485380ad253b31ea7d36fd3ab9afc68946b9
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-16 12:23:09 +1100

    1.[ADD]:add noncopyable class to forbid shadow and deep copy

---
+++ include/service/Noncopyable.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _NONCOPYABLE_HPP_
+#define _NONCOPYABLE_HPP_
+
+namespace SoftRasterizer {
+          class Noncopyable {
+          protected:
+                    Noncopyable() {};
+                    ~Noncopyable() {};
+          private:
+                    Noncopyable(const Noncopyable&);
+                    const Noncopyable& operator=(const Noncopyable&);
+          };
+}
+
+#endif _NONCOPYABLE_HPP_
