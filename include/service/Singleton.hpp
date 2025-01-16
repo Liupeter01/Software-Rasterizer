3a1fa23a0105a884a95b6c44212ae7b082acc885
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-16 12:22:07 +1100

    1.[ADD]:add singleton design partern

---
+++ include/service/Singleton.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef _SINGLETON_HPP
+#define _SINGLETON_HPP
+#include <memory>
+#include <mutex>
+
+namespace SoftRasterizer {
+
+          template <typename _Ty> struct SingletonDeletor {
+                    void operator()(_Ty* value) { delete value; }
+          };
+
+          template <typename _Ty> class Singleton {
+                    static std::shared_ptr<_Ty> m_instance;
+
+                    /*Once we delete move ctor then all copy ctor functions are deleted!*/
+                    Singleton(Singleton&&) = delete;
+
+          protected:
+                    Singleton() = default;
+
+          public:
+                    ~Singleton() {}
+                    static std::shared_ptr<_Ty>& get_instance() {
+                              static std::once_flag flag;
+                              std::call_once(flag, [&]() {
+                                        m_instance = std::shared_ptr<_Ty>(new _Ty, SingletonDeletor<_Ty>());
+                                        });
+                              return m_instance;
+                    }
+          };
+
+          template <typename _Ty>
+          std::shared_ptr<_Ty> Singleton<_Ty>::m_instance = nullptr;
+};
+#endif // _SINGLETON_HPP
