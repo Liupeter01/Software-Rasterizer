d7e7f923eb58e83ba665f33d3652e2ab8c85f53c
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-18 07:03:07 +1100

    1.[ADD]:add glm version triangle def 2.[REMOVE]:remove eigen code

--- include/Triangle.hpp
+++ include/Triangle.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #ifndef _TRIANGLE_HPP_
 #define _TRIANGLE_HPP_
-#include <Eigen/Eigen>
 #include <array>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
 #include <initializer_list>
 
 namespace SoftRasterizer {
@@ -10,6 +11,13 @@ struct alignas(32) Triangle {
   Triangle();
 
   struct BoundingBox {
+            BoundingBox()
+                      :startY(0)
+                      ,startX(0)
+                      ,endX(0)
+                      ,endY(0)
+            { }
+
     long long startX;
     long long startY;
 
@@ -17,29 +25,24 @@ struct alignas(32) Triangle {
     long long endY;
   };
 
-  const Eigen::Vector3f &a() const { return m_vertex[0]; }
-  const Eigen::Vector3f &b() const { return m_vertex[1]; }
-  const Eigen::Vector3f &c() const { return m_vertex[2]; }
+  const glm::vec3& a() const { return m_vertex[0]; }
+  const glm::vec3& b() const { return m_vertex[1]; }
+  const glm::vec3& c() const { return m_vertex[2]; }
 
-  void setVertex(std::initializer_list<Eigen::Vector3f> _vertex);
-  void setNormal(std::initializer_list<Eigen::Vector3f> _normal);
-  void setColor(std::initializer_list<Eigen::Vector3i> _color);
-  void setTexCoord(std::initializer_list<Eigen::Vector2f> _texCoords);
+  void setVertex(std::initializer_list<glm::vec3> _vertex);
+  void setNormal(std::initializer_list<glm::vec3> _normal);
+  void setColor(std::initializer_list<glm::vec3> _color);
+  void setTexCoord(std::initializer_list<glm::vec2> _texCoords);
+
   void calcBoundingBox(const std::size_t width, const std::size_t height);
   bool isOverlapping(const Triangle &box1, const Triangle &box2) const;
 
   /*the original coordinates of the triangle, v0, v1, v2 in counter clockwise
    * order*/
-  std::array<Eigen::Vector3f, 3> m_vertex;
-
-  // Color for each vertex
-  std::array<Eigen::Vector3i, 3> m_color;
-
-  // texture u,v coordinates for each vertex
-  std::array<Eigen::Vector2f, 3> m_texCoords;
-
-  // normal vector for each vertex
-  std::array<Eigen::Vector3f, 3> m_normal;
+  std::array<glm::vec3, 3> m_vertex;
+  std::array<glm::vec3, 3> m_color;                          // Color for each vertex
+  std::array<glm::vec2, 3> m_texCoords;                  // texture u,v coordinates for each vertex
+  std::array<glm::vec3, 3> m_normal;                       // normal vector for each vertex
 
   /*BoundBox Calculation!*/
   BoundingBox box;
