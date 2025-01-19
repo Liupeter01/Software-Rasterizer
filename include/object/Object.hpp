8d12f442e0c3a26ac0fab12b6e0cf19e34de9dcf
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-19 21:45:49 +1100

    1.[ADD]:add object class as a base class with virtual function

---
+++ include/object/Object.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _OBJECT_HPP_
+#define _OBJECT_HPP_
+#include <bvh/Bounds3.hpp>
+
+namespace  SoftRasterizer {
+          class Object {
+          public:
+                    Object() {}
+                    virtual ~Object() {}
+                    virtual Bounds3 getBounds() = 0;
+          };
+}
+
+#endif //_OBJECT_HPP_
