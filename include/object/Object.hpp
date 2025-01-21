81d6b72c0cfc2e38b5349ef518a71c21fce8ade0
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-21 12:54:47 +1100

    1.[ADD]:add virtual function to detect intersect with object

--- include/object/Object.hpp
+++ include/object/Object.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #ifndef _OBJECT_HPP_
 #define _OBJECT_HPP_
+#include <optional>
+#include <ray/Ray.hpp>        //ray def
 #include <bvh/Bounds3.hpp>
+#include <ray/Intersection.hpp>
 
 namespace SoftRasterizer {
-class Object {
-public:
+struct Object {
   Object() {}
   virtual ~Object() {}
   virtual Bounds3 getBounds() = 0;
+  virtual bool intersect(const Ray&) = 0;
+  virtual bool intersect(const Ray&, float&) = 0;
+  virtual Intersection getIntersect(Ray&) = 0;
 };
 } // namespace SoftRasterizer
 
