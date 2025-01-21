3adcd4116dda7d5d4d5e4e3d64137190b9a707f9
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-21 12:56:45 +1100

    1.[ADD]:add intersection def 2.[ELEMENTS]:intersected flag, intersected coordinates, the normal on intersected point, intersected time and obj

---
+++ include/ray/Intersection.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef _INTERSECTION_HPP_
+#define _INTERSECTION_HPP_
+#include <glm/glm.hpp>
+
+namespace SoftRasterizer {
+
+          /*declaration*/
+          struct Object;
+
+          struct Intersection {
+                    Intersection()
+                              : intersected(false)
+                              , coords(glm::vec3(0.f))
+                              , normal(glm::vec3(0.f))
+                              , intersect_time(std::numeric_limits<float>::max())
+                              , obj(nullptr)
+                    {
+                    }
+
+                    bool intersected;
+                    float intersect_time;
+                    glm::vec3 coords;
+                    glm::vec3 normal;
+                    Object* obj;
+          };
+}
+
+#endif //_INTERSECTION_HPP_
