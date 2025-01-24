4104939d4a29b8d8f7e077b3d89d6e8d6ec6d22e
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-24 12:58:53 +1100

    1.[ADD]:add index inside intersection class to indicate which normal and UV should be utlized on this intersection point

--- include/ray/Intersection.hpp
+++ include/ray/Intersection.hpp
@@ -10,10 +10,12 @@ struct Object;
 struct Material;
 
 struct Intersection {
-  Intersection()
-      : intersected(false), coords(glm::vec3(0.f)), normal(glm::vec3(0.f)),
-        intersect_time(std::numeric_limits<float>::max()), obj(nullptr) ,material(nullptr){}
+          Intersection()
+                    : intersected(false), coords(glm::vec3(0.f)), normal(glm::vec3(0.f)), index(0),
+                    intersect_time(std::numeric_limits<float>::max()), obj(nullptr), material(nullptr) {
+          }
 
+  std::size_t index;
   bool intersected;
   float intersect_time;
   glm::vec3 coords;
