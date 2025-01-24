9c63559da1adbfbda5ea251f166a1b16ca9ab002
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-24 12:54:58 +1100

    1.[ADD]:add index argument inside virtual class to indicate the index of triangle inside mesh

--- include/object/Object.hpp
+++ include/object/Object.hpp
@@ -16,12 +16,15 @@ struct Object {
                     bool valid = false;
           };
 
+          std::size_t index = 0;
+
   Object() {}
   virtual ~Object() {}
   virtual Bounds3 getBounds() = 0;
   virtual bool intersect(const Ray &) = 0;
   virtual bool intersect(const Ray &, float &) = 0;
   virtual Intersection getIntersect(Ray &) = 0;
+  virtual glm::vec3 getDiffuseColor(const glm::vec2& uv) = 0;
   virtual Properties getSurfaceProperties(const std::size_t faceIndex, 
                                                                 const glm::vec3& Point, 
                                                                 const glm::vec3& viewDir,  
