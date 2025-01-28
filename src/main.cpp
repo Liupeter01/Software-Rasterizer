ff383bdeb0446f6fb20243e7a62c9e9df579c7ec
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-01-28 16:45:33 +1100

    1.[TEST]:add more lights 2.[TEST]:modify bunny's arguments 3.[NOT TESTED & COMMENT]:Sphere obj

--- src/main.cpp
+++ src/main.cpp
@@ -19,25 +19,26 @@ int main() {
       /*up=*/glm::vec3(0.0f, 1.0f, 0.0f),
       /*background color*/ glm::vec3(0.235294, 0.67451, 0.843137));
 
-  // auto diffuse_sphere = std::make_unique<SoftRasterizer::Sphere>(
-  //          /*center=*/glm::vec3(0.8f, 0.f, 0.f),
-  //          /*radius=*/0.1f);
-  //
-  // diffuse_sphere->getMaterial()->type =
-  // SoftRasterizer::MaterialType::DIFFUSE_AND_GLOSSY;
-  // diffuse_sphere->getMaterial()->color = glm::vec3(0.6f, 0.7f, 0.8f); /*Set
-  // Diffuse Color*/
+  /*Set Diffuse Color*/
+   auto diffuse_sphere = std::make_unique<SoftRasterizer::Sphere>(
+            /*center=*/glm::vec3(0.8f, 0.f, 0.f),
+            /*radius=*/0.1f);
 
-  // auto reflect_sphere = std::make_unique<SoftRasterizer::Sphere>(
-  //           /*center=*/glm::vec3(-0.8f, 0.f, 0.f),
-  //           /*radius=*/0.15f);
+   diffuse_sphere->getMaterial()->type =
+             SoftRasterizer::MaterialType::DIFFUSE_AND_GLOSSY;
+   diffuse_sphere->getMaterial()->color = glm::vec3(0.6f, 0.7f, 0.8f);
 
-  // reflect_sphere->getMaterial()->type =
-  // SoftRasterizer::MaterialType::REFLECTION_AND_REFRACTION;
-  // reflect_sphere->getMaterial()->ior = 1.49; /*Air to Glass*/
+   /*Set Reflect Color*/
+   auto reflect_sphere = std::make_unique<SoftRasterizer::Sphere>(
+             /*center=*/glm::vec3(-0.8f, 0.f, 0.f),
+             /*radius=*/0.15f);
 
-  // scene->addGraphicObj(std::move(diffuse_sphere), "diffuse");
-  // scene->addGraphicObj(std::move(reflect_sphere), "reflect");
+   reflect_sphere->getMaterial()->type =
+   SoftRasterizer::MaterialType::REFLECTION_AND_REFRACTION;
+   reflect_sphere->getMaterial()->ior = 1.49; /*Air to Glass*/
+
+   //scene->addGraphicObj(std::move(diffuse_sphere), "diffuse");
+   //scene->addGraphicObj(std::move(reflect_sphere), "reflect");
 
   scene->addGraphicObj(CONFIG_HOME "examples/models/bunny/bunny.obj", "bunny",
                        glm::vec3(0, 1, 0), 0.f, glm::vec3(0.f), glm::vec3(1.f));
@@ -48,18 +49,23 @@ int main() {
   auto bunny_obj = scene->getMeshObj("bunny");
   bunny_obj.value()->getMaterial()->type =
       SoftRasterizer::MaterialType::DIFFUSE_AND_GLOSSY;
-  bunny_obj.value()->getMaterial()->color = glm::vec3(1.f);
-  bunny_obj.value()->getMaterial()->Kd = glm::vec3(1.f);
-  bunny_obj.value()->getMaterial()->Ka = glm::vec3(0.005f);
+  bunny_obj.value()->getMaterial()->color = glm::vec3(1.0f);
+  bunny_obj.value()->getMaterial()->Kd = glm::vec3(1.0f);
+  bunny_obj.value()->getMaterial()->Ka = glm::vec3(0.015f);
   bunny_obj.value()->getMaterial()->Ks = glm::vec3(0.7937f);
   bunny_obj.value()->getMaterial()->specularExponent = 150.f;
 
   /*Add Light To Scene*/
   auto light1 = std::make_shared<SoftRasterizer::light_struct>();
-  light1->position = glm::vec3{0.3f, 0.3f, -0.3f};
-  light1->intensity = glm::vec3{10, 10, 10};
+  light1->position = glm::vec3{0.5f, -0.4f, -0.9f};
+  light1->intensity = glm::vec3{1, 1, 1};
+
+  auto light2 = std::make_shared<SoftRasterizer::light_struct>();
+  light2->position = glm::vec3{ -0.5f, -0.4f, -0.9f };
+  light2->intensity = glm::vec3{ 1, 1, 1 };
 
   scene->addLight("Light1", light1);
+  scene->addLight("Light2", light2);
 
   /*Register Scene To Render Main Frame*/
   render->addScene(scene);
@@ -80,7 +86,7 @@ int main() {
 
     /*View Matrix*/
     scene->setViewMatrix(
-        /*eye=*/glm::vec3(0.0f, -0.3f, -0.7f),
+        /*eye=*/glm::vec3(0.0f, 0.0f, -0.7f),
         /*center=*/glm::vec3(0.0f, 0.0f, 0.0f),
         /*up=*/glm::vec3(0.0f, 1.0f, 0.0f));
 
