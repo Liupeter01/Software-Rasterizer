2114d5ef4012f6698750e1c086684f56e1d368e3
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-02-07 12:05:23 +1100

    1.[ADD]:add source file 2.[ADD]:add self emission variable in material

--- include/object/Material.hpp
+++ include/object/Material.hpp
@@ -14,60 +14,38 @@ enum class MaterialType {
 };
 
 struct Material {
-  Material(MaterialType _type = MaterialType::REFLECTION_AND_REFRACTION,
-           const glm::vec3 &_Ka = glm::vec3(0.0f),
-           const glm::vec3 &_Kd = glm::vec3(0.0f),
-           const glm::vec3 &_Ks = glm::vec3(0.0f),
-           const float _specularExponent = 0.0f)
-
-      : type(_type), Ka(_Ka), Kd(_Kd), Ks(_Ks), Ns(0.f), Ni(0.f), d(0.f),
-        illum(0), ior(0.f), specularExponent(_specularExponent), map_Ka(""),
-        map_Kd(""), map_Ks(""), map_Ns(""), map_d(""), map_bump("") {}
+          Material(MaterialType _type = MaterialType::REFLECTION_AND_REFRACTION,
+                    const glm::vec3& _Ka = glm::vec3(0.0f),
+                    const glm::vec3& _Kd = glm::vec3(0.0f),
+                    const glm::vec3& _Ks = glm::vec3(0.0f),
+                    const float _specularExponent = 0.0f,
+                    const glm::vec3& _emission = glm::vec3(0.f));
 
   MaterialType getMaterialType() const { return type; }
 
   // uniform sample on the hemisphere
-  const glm::vec3& sample(const glm::vec3& wi, const glm::vec3& N) {
-            if (type == MaterialType::DIFFUSE_AND_GLOSSY) {
-                      /*
-                      * Generator 2D Random Sample Coordinates
-                      * x ^ 2 + y ^ 2 + z ^ 2 = 1 = r^2 + z^2 = 1
-                      */
-                      float z = std::abs(1.f - 2.f * Tools::random_generator());  //Generate Z |[-1, 1]|=>[0, 1]
-                      float r = std::sqrt(1.f - z * z);     //on X and Y Axis
-                      float phi = 2.0f * Tools::PI * Tools::random_generator(); // angle [0, 2PI]
-
-                      /*Generate a ray from (0, 0, 0) to (x, y, z)*/
-                      glm::vec3 local(r * std::cos(phi), r * std::sin(phi), z);
-
-                      /* Mathematical Transformation Principle
-                       * localRay = (x, y, z) => worldRay = xT+yB+zN*/
-                      return Tools::toWorld(local, N);
-            }
-            return glm::vec3(0.f);
-  }
+  const glm::vec3& sample(const glm::vec3& wi, const glm::vec3& N);
 
   /*
   * Given an incident direction, an outgoing direction, and a normal vector, 
   * calculate the probability density of obtaining the outgoing direction using the sampling method.
   * uniform sample probability 1 / (2 * PI) = 0.5f * PI_INV
   */
-  const float pdf(const glm::vec3& wi, const glm::vec3& wo, const glm::vec3& N) {
-            return glm::dot(wi, N) > 0 ? uniform_sampling_on_sphere : 0.f;
-  }
+  const float pdf(const glm::vec3& wi, 
+                          const glm::vec3& wo, 
+                          const glm::vec3& N);
 
-  /* Sample a ray by material properties */
 
   /* 
    * Given an incident direction, an outgoing direction, and a normal vector, 
    *  calculate the contribution of this ray , which is Fr(P, wi, wo)
    */
-  const glm::vec3& fr_contribution(const glm::vec3& wi, const glm::vec3& wo, const glm::vec3& N) {
-            if (type == MaterialType::DIFFUSE_AND_GLOSSY) {
-                      return glm::dot(wi, N) > 0 ? Kd * Tools::PI_INV : glm::vec3(0.f);
-            }
-            return glm::vec3(0.f);
-  }
+  const glm::vec3& fr_contribution(const glm::vec3& wi, 
+                                                         const glm::vec3& wo, 
+                                                         const glm::vec3& N);
+
+  const glm::vec3& getEmission() const { return emission; }
+  [[nodiscard]] const bool hasEmission();
 
   std::string name;       // Material Name
   MaterialType type;      // Material Type
@@ -86,6 +64,9 @@ struct Material {
   std::string map_Ns;     // Specular Hightlight Map
   std::string map_d;      // Alpha Texture Map
   std::string map_bump;   // Bump Map
+
+  //Self Emissive object
+  glm::vec3 emission;
 
   //Unit Sphere's radius
   static constexpr float radius = 1.0f;       
