b2ec86f512c9d0d27f6c5969b2ce64eec8a033ec
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-02-07 20:22:22 +1100

    1.[MODIFY]:modify argument passing rules, because of the changes inside scene class

--- src/PathTracing.cpp
+++ src/PathTracing.cpp
@@ -24,30 +24,24 @@ void SoftRasterizer::PathTracing::draw(Primitive type) {
                      */
                     SceneObj->updatePosition();
 
-                    std::vector<SoftRasterizer::light_struct> lights = SceneObj->loadLights();
                     const glm::vec3 eye = SceneObj->loadEyeVec();
 
                     float scale = std::tan(glm::radians(SceneObj->m_fovy * 0.5));
 
                     oneapi::tbb::parallel_for(
-                              oneapi::tbb::blocked_range2d<std::size_t>(0, m_height, 16, 0, m_width,
-                                        16), // 16x16 Block
+                              oneapi::tbb::blocked_range2d<std::size_t>(0, m_height, 16, 0, m_width, 16), // 16x16 Block
                               [&](const oneapi::tbb::blocked_range2d<std::size_t>& range) {
-                                        for (std::size_t ry = range.rows().begin(); ry < range.rows().end();
-                                                  ++ry) {
-                                                  for (std::size_t rx = range.cols().begin(); rx < range.cols().end();
-                                                            ++rx) {
+                                        for (std::size_t ry = range.rows().begin(); ry < range.rows().end(); ++ry) {
+                                                  for (std::size_t rx = range.cols().begin(); rx < range.cols().end(); ++rx) {
 
-                                                            float x = (2 * (rx + 0.5f) / static_cast<float>(m_width) - 1) *
-                                                                      aspect_ratio * scale;
-                                                            float y =
-                                                                      (2 * (ry + 0.5f) / static_cast<float>(m_height) - 1) * scale;
+                                                            float x = (2 * (rx + 0.5f) / static_cast<float>(m_width) - 1) * aspect_ratio * scale;
+                                                            float y = (2 * (ry + 0.5f) / static_cast<float>(m_height) - 1) * scale;
 
                                                             try {
                                                                       Ray ray(eye, glm::normalize(glm::vec3(x, y, 0) - eye));
                                                                       glm::vec3 color = glm::vec3(0.f);
                                                                       for (std::size_t i = 0; i < sample; ++i) {
-                                                                                color += SceneObj->pathTracingSerial(ray, 0, lights) / glm::vec3(sample);
+                                                                                color += SceneObj->pathTracing(ray) / glm::vec3(sample);
                                                                       }
                                                                       writePixel(rx, ry, Tools::normalizedToRGB(color));
                                                             }
